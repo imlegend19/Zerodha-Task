8b655f410485c14be695688036898f0b9f4d5456
mahen-swiggy <mahen.mg@swiggy.in>
2021-04-23 20:20:43 +0530

    modified CACHE settings

--- ZerodhaTask/settings.py
+++ ZerodhaTask/settings.py
@@ -140,9 +140,6 @@ CACHES = {
         "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "CONNECTION_POOL_KWARGS": {
-                "ssl_cert_reqs": None
-            },
         }
     }
 }

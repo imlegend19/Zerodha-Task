951554ad729410a845070acfe6141cafe4669720
mahen-swiggy <mahen.mg@swiggy.in>
2021-04-23 21:59:15 +0530

    added fetch_all view

--- ZerodhaTask/app/urls.py
+++ ZerodhaTask/app/urls.py
@@ -5,5 +5,6 @@ from ZerodhaTask.app.views import *
 
 urlpatterns = [
     path('api/search/<str:query>', search),
+    path('api/fetch_all', fetch_all),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
--- ZerodhaTask/app/views.py
+++ ZerodhaTask/app/views.py
@@ -3,10 +3,9 @@ from django.http import JsonResponse
 from ZerodhaTask import REDIS
 
 
-def search(request, query):
-    print(f"got query for {query}")
+def get_results(s):
     results = []
-    for key in REDIS.scan_iter(f"*{query.upper()}*"):
+    for key in REDIS.scan_iter(s):
         field_dict = REDIS.hgetall(key)
 
         results.append({
@@ -18,8 +17,18 @@ def search(request, query):
             'close': field_dict[b'close'].decode(),
         })
 
-    print(f"found {len(results)}")
+    return results
 
+
+def search(request, query):
     return JsonResponse(
-        results, safe=False
+        get_results(f"*{query.upper()}*"),
+        safe=False
+    )
+
+
+def fetch_all(request):
+    return JsonResponse(
+        get_results('*'),
+        safe=False
     )

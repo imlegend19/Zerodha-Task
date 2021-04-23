30cd395049ee32338f42d1a0301f5a76d8c681bd
Mahen Gandhi <mahengandhi19@gmail.com>
2021-04-24 00:04:51 +0530

    Changed redis-url

--- ZerodhaTask/__init__.py
+++ ZerodhaTask/__init__.py
@@ -1,9 +1,11 @@
-import os
 from urllib.parse import urlparse
 
 import redis
 
-redis_url = urlparse(os.environ.get("REDIS_URL"))
+redis_url = urlparse(
+    'redis://:p97b4319d2a1b9515a467c740ba02fd0d23923a3cece162a1238b46bcba354350@ec2-18-215-213-65.compute-1.amazonaws'
+    '.com:32570 '
+)
 
 REDIS = redis.Redis(
     host=redis_url.hostname,
